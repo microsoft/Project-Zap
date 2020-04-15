d5b5e9fb343ef62f2caddfe088b3b77460f4200a
Jim Paine <jim@jimpaine.io>
2020-04-15 15:20:30 +0100

    Remove unused using

--- Project.Zap.Library/Services/OrganizationRepository.cs
+++ Project.Zap.Library/Services/OrganizationRepository.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Net;
 using System.Threading.Tasks;
 
 namespace Project.Zap.Library.Services
