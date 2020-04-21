9ebc3345a36729ca5f8bc340fffcc626943bca07
Jim Paine <jim@jimpaine.io>
2020-04-21 10:13:42 +0100

    added maps to controller

--- Project.Zap/Controllers/ShiftsController.cs
+++ Project.Zap/Controllers/ShiftsController.cs
@@ -22,18 +22,20 @@ namespace Project.Zap.Controllers
         private readonly IRepository<Location> locationRepository;
         private readonly Microsoft.Graph.IGraphServiceClient graphServiceClient;
         private readonly IStringLocalizer<ShiftsController> stringLocalizer;
-
+        private readonly IMapService mapService;
 
         public ShiftsController(
             IRepository<Shift> shiftRepository, 
             IRepository<Location> locationRepository, 
             Microsoft.Graph.IGraphServiceClient graphServiceClient,
-            IStringLocalizer<ShiftsController> stringLocalizer)
+            IStringLocalizer<ShiftsController> stringLocalizer,
+            IMapService mapService)
         {
             this.shiftRepository = shiftRepository;
             this.locationRepository = locationRepository;
             this.graphServiceClient = graphServiceClient;
             this.stringLocalizer = stringLocalizer;
+            this.mapService = mapService;
         }
 
         public async Task<IActionResult> Index()
